78144716923da87df3e010c258f1a80b67425a7c
Eric Calonge <Eric Calonge>
2016-04-02 00:30:18 -0700

    Update

--- hacky.py
+++ hacky.py
@@ -1,11 +1,21 @@
 import sys
+import os.path
 
-sys.argv
-f = open('story1', 'a+')
-line = f.file()
-while line:
-	line = f.readline()
-print(line)
-var = input("Next Line:")
-var.replace('\n', ' ')
+if len(sys.argv) != 2:
+	sys.exit(1)
+workingfile = sys.argv[1].replace(' ', '')
+workingfile.replace('\\\'', '')
+workingfile_last = workingfile + "last"
+f = open(workingfile, 'a')
+num = 0
+if os.path.isfile(workingfile_last):
+	r = open(workingfile_last, 'r')
+	line = r.readline().replace("\n", "")
+	num = int(r.readline())
+	num += 1
+	print(line + "" + str(num))
+	r.close()
+r = open(workingfile_last, 'w')
+var = raw_input('Next Line: ')
+r.write(var + "\n" + str(num))
 f.write(var)
